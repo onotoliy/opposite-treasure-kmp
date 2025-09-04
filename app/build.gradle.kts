9f15599115c213867c9d4805241b67b3e2c14c51
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2025-09-04 18:32:31 +0300

    init project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,9 @@
 @file:Suppress("OPT_IN_IS_NOT_ENABLED")
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -16,8 +21,24 @@ kotlin {
 
     jvm("desktop")
 
-    js(IR) {
-        browser()
+
+    wasmJs {
+//        moduleName = "app"
+        binaries.executable()
+        browser {
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                outputFileName = "app.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(rootDirPath)
+                        add(projectDirPath)
+                    }
+                }
+            }
+        }
     }
 
     sourceSets {
@@ -32,15 +53,18 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.components.resources)
+            implementation("org.jetbrains.skiko:skiko:0.9.24")
+        }
+
+        wasmJsMain.dependencies {
+            implementation (compose.ui)
+            implementation (compose.foundation)
+            implementation("org.jetbrains.skiko:skiko-wasm-js:0.9.24")
         }
 
         androidMain.dependencies {
 
         }
-
-//        desktopMain.dependencies {
-//
-//        }
     }
 }
 
