35da8334047ce5bc249bb7f4658ae0786313d86d
Anatoliy Pokhresniy <a.k.pohresniy@gmail.com>
2025-09-24 17:25:20 +0300

    init project

---
+++ app/js/src/jsMain/kotlin/com/github/onotoliy/opposite/ui/main.kt
@@ -0,0 +1,28 @@
+package com.github.onotoliy.opposite.ui
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import androidx.navigation.ExperimentalBrowserHistoryApi
+import androidx.navigation.bindToBrowserNavigation
+import com.github.onotoliy.opposite.di.adapterModel
+import com.github.onotoliy.opposite.di.networkModule
+import com.github.onotoliy.opposite.di.repositoryModule
+import com.github.onotoliy.opposite.di.viewModelModule
+import com.github.onotoliy.opposite.ui.navigation.WebWindowNavigation
+import org.jetbrains.skiko.wasm.onWasmReady
+import org.koin.compose.KoinApplication
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
+fun main() {
+    onWasmReady {
+        ComposeViewport("composeApp") {
+            KoinApplication(application = {
+                modules(networkModule, repositoryModule, viewModelModule, adapterModel)
+            }) {
+                WebWindowNavigation() {
+                    it.bindToBrowserNavigation()
+                }
+            }
+        }
+    }
+}
